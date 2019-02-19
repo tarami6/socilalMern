d015bb85c1c4b7b3485fc37a0d3299a95c300488
tarami6 <allindn@gmail.com>
2019-02-19 16:04:15 +0200

    first commit

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:
+        'mongodb+srv://brad:brad@cluster0-wixly.mongodb.net/social?retryWrites=true',
+    secretKey: 'secret'
+}
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+// Body Parser
+app.use(bodyParser.urlencoded({extends : false}));
+app.use(bodyParser.json());
+
+// Db Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDb
+mongoose
+    .connect(db )
+    .then(()=> console.log("mongo Connected"))
+    .catch((err)=> console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require('./config/passport')(passport);
+
+
+// Use Routes
+app.use('/api/users',users);
+app.use('/api/profile',profile);
+app.use('/api/posts',posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=> console.log(`Server running on port ${port}`));
